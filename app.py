49b46e44a3bed6b14226b71e474f1458aaad1084
22p26c0377 <pawut.j@gmail.com>
2021-08-26 05:49:25 +0800

    add section2

--- app.py
+++ app.py
@@ -3,3 +3,33 @@ import streamlit as st
 # working with sample data.
 import numpy as np
 import pandas as pd
+import altair as alt
+from vega_datasets import data
+
+
+# section2
+
+df = pd.DataFrame({
+    'xval': ["1", "1", "2-3", "2-3", "4-5", "4-5", "6-10", "6-10", "11+", "11+"],
+    'group': [1, 2, 1, 2, 1, 2, 1, 2, 1, 2],
+    "value": [5000, 8000, 500, 3000, 100, 1000, 60, 800, 30, 300]
+})
+
+c = alt.Chart(df).mark_bar().encode(
+    x=alt.X('group:O', axis=alt.Axis(title=None, labels=False, ticks=False)),
+    y=alt.Y('value:Q', axis=alt.Axis(grid=False)),
+    color='group:N',
+    column=alt.Column(
+        'xval:N',
+        sort=["1", "2-3", "4-5", "6-10", "11+"],
+        header=alt.Header(title=None, labelOrient='bottom')
+    )
+).configure_view(
+    stroke='transparent'
+)
+
+st.altair_chart(c)
+##############################################################
+
+############################################################
+st.button("Re-run")
